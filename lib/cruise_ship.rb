4a6be72226f25e8ec057700d8f9f7efa7f7dfd89
Jack Cooper <jackcooper1245@gmail.com>
2020-04-09 15:04:04 +0000

    Automatically backed up by Learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -7,11 +7,11 @@
 # }
 
 def select_winner(passengers)
-  # add the code snippet here!
   winner = ""
-  passenger.each do |suite, name|
-    if suite == suite_a && name[0] == "A"
-      winner = name
-    end
+passengers.each do |suite, name|
+  if suite == :suite_a && name[0] =="A"
+    winner = name
+  end
 end
+ 
 winner
