bc4094e6939f0adced40e121ddd79333a6e2bf88
Jack Cooper <jackcooper1245@gmail.com>
2020-04-09 15:05:28 +0000

    Automatically backed up by Learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -1,5 +1,5 @@
- #passengers = {
- #suite_a: "Amanda Presley", 
+# passengers = {
+# suite_a: "Amanda Presley", 
 # suite_b: "Seymour Hoffman", 
 # suite_c: "Alfred Tennyson", 
 # suite_d: "Charlie Chaplin", 
@@ -7,11 +7,5 @@
 # }
 
 def select_winner(passengers)
- winner = ""
-passengers.each do |suite, name|
-  if suite == :suite_a && name.start_with?("A")
-    winner = name
-  end
+  # add the code snippet here!
 end
- end
- winner
